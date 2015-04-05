9e94f10ae5d83c5a943fa6c5cd50bbe6aa1f7d35
snackthyme <stevenboddez@gmail.com>
2015-04-05 11:49:01 -0600

    quick main menu fix

--- interfaces/main_menu.py
+++ interfaces/main_menu.py
@@ -6,9 +6,10 @@ class Main_Menu(Interface):
         super().__init__(gui)
 
     def update(self, screen, dt):
-        if self.players[0].get_input()[4]:
-            self.gui.interfaces.remove(self)
-            self.gui.interfaces.append(Main_Game(self.gui))
+        if self.players:
+            if self.players[0].get_input()[4]:
+                self.gui.interfaces.remove(self)
+                self.gui.interfaces.append(Main_Game(self.gui))
 
     def draw(self, screen):
         screen.fill(self.gui.WHITE)
